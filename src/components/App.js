2897c9f8b87f774645725e42a9e0d971ad17ae19
Ritu0097 <rituchaudhary0097@gmail.com>
2024-04-07 15:18:50 +0530

    counter

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,20 @@
 import React from "react";
 import './../styles/App.css';
 
-const App = () => {
+const CounterComponent = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <button onClick={handleClick}>Click me</button>
+      <p>Button clicked {count} times.</p>
     </div>
-  )
-}
+  );
+};
+
 
 export default App
